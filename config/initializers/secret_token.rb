d56f660ac7d826f7323d2ad43ade33fa9a93cac8
Samuel Albert Meister <samuelalbertmeister@gmail.com>
2013-09-13 11:30:36 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitnessbuzz::Application.config.secret_key_base = '4d80dec2673b723f83b16d84f5204b71d4e7099a625194c8fcdca6972cb25d6d7944988e5e79d657766a30b74c6b53864caf1db6a964cde2941c7e07009f0908'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fitnessbuzz::Application.config.session_store :cookie_store, key: '_fitnessbuzz_session'
